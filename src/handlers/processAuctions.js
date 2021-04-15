f900237f6b7552a4184d2193687981f3cf1298e6
nsjosed <navarro_21_bronw@hotmail.com>
2021-04-15 11:52:24 +0200

    processAuction checks and closes ended Auct

--- src/handlers/processAuctions.js
+++ src/handlers/processAuctions.js
@@ -1,9 +1,27 @@
 import { getEndedAuctions } from '../lib/getEndedAuctions';
+import { closeAuction } from '../lib/closeAuction';
+import createError from 'http-errors';
 
 async function processAuctions(event, context) {
 
-    const auctionsToClose = await getEndedAuctions();
-    console.log(auctionsToClose);
+    try {
+        // get array of closed auctions
+        const auctionsToClose = await getEndedAuctions();
+
+        // For each iteration it returns the promise result of each auction that needs to be closed
+        const closePromises = auctionsToClose.map(auction => closeAuction(auction));
+
+        // It waits for all promises to be finished
+        await Promise.all(closePromises);
+
+        // this function is NOT triggered by API Gateway so it can return anything (otherwise HTTP-structure)
+        return { closed: closePromises.length };
+
+    } catch (error) {
+        console.error(error);
+        throw new createError.InternalServerError(error);
+    }
+
 }
 
 export const handler = processAuctions;
