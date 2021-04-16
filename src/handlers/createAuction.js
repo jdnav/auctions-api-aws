b7696af92607a837015e927900f702e320423d5f
nsjosed <navarro_21_bronw@hotmail.com>
2021-04-16 19:58:59 +0200

    createAuction schema

--- src/handlers/createAuction.js
+++ src/handlers/createAuction.js
@@ -1,43 +1,36 @@
 import { v4 as uuid } from 'uuid';
 import AWS from 'aws-sdk';
-import commonMiddleware from '../lib/commonMiddleware';
 import createError from 'http-errors';
+import validator from '@middy/validator';
+import commonMiddleware from '../lib/commonMiddleware';
+import createAuctionSchema from '../lib/schemas/createAuctionSchema';
 
-// Creates a DynamoDB document client
 const dynamodb = new AWS.DynamoDB.DocumentClient();
 
 async function createAuction(event, context) {
-
-  // const body = JSON.parse(event.body);
   const { title } = event.body;
-
   const now = new Date();
   const endDate = new Date();
-  // The auction will be ended after 1h
   endDate.setHours(now.getHours() + 1);
 
   const auction = {
-    //title: body.tile
     id: uuid(),
     title,
     status: 'OPEN',
     createdAt: now.toISOString(),
-    endingAt: endDate,
+    endingAt: endDate.toISOString(),
     highestBid: {
       amount: 0,
-    }
-  }
+    },
+  };
 
   try {
-    // Create the new item in the db
     await dynamodb.put({
       TableName: process.env.AUCTIONS_TABLE_NAME,
       Item: auction,
     }).promise();
-
   } catch (error) {
     console.error(error);
-    // it creates the error object
     throw new createError.InternalServerError(error);
   }
 
@@ -47,7 +40,5 @@ async function createAuction(event, context) {
   };
 }
 
-// Function wrapped in commonMiddleware
-export const handler = commonMiddleware(createAuction);
-
-
+export const handler = commonMiddleware(createAuction)
+//  .use(validator({ inputSchema: createAuctionSchema }));
