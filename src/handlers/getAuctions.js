f75919a01cde85ac12f5d6cd42e8f59b46610f7e
nsjosed <navarro_21_bronw@hotmail.com>
2021-04-16 19:19:28 +0200

    getAuctions func uses schema

--- src/handlers/getAuctions.js
+++ src/handlers/getAuctions.js
@@ -1,6 +1,8 @@
 import AWS from 'aws-sdk';
 import commonMiddleware from '../lib/commonMiddleware';
 import createError from 'http-errors';
+import validator from '@middy/validator';
+import getAuctionsSchema from '../lib/schemas/getAuctionsSchema';
 
 // Creates a DynamoDB document client
 const dynamodb = new AWS.DynamoDB.DocumentClient();
@@ -47,4 +49,11 @@ async function getAuctions(event, context) {
 }
 
 // Function wrapped in commonMiddleware
-export const handler = commonMiddleware(getAuctions);
+export const handler = commonMiddleware(getAuctions)
+    .use(validator(
+        {
+            inputSchema: getAuctionsSchema,
+            useDefaults: true,
+            strictTypes: false // added
+        }
+    ));
